2b8b48b5a141c833289900f0b4e689334a8729a1
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-01-30 11:34:11 +0000

    Done.

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,4 +1,4 @@
-def greeting
-  puts "Hi, #{Bekah}! Welcome to the wonderful world of Ruby programming."
+def greeting(name)
+  puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
 end
 
